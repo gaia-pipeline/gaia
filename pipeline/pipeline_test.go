6c533a584b32787f1dc26103cebd662333a90483
bidhan-a <bidhan619@gmail.com>
2018-07-19 11:20:05 +0545

    add more tests for pipeline package

--- pipeline/pipeline_test.go
+++ pipeline/pipeline_test.go
@@ -7,6 +7,24 @@ import (
 	"github.com/gaia-pipeline/gaia"
 )
 
+func TestAppend(t *testing.T) {
+	ap := NewActivePipelines()
+
+	p1 := gaia.Pipeline{
+		Name:    "Pipeline A",
+		Type:    gaia.PTypeGolang,
+		Created: time.Now(),
+	}
+	ap.Append(p1)
+
+	ret := ap.GetByName("Pipeline A")
+
+	if p1.Name != ret.Name || p1.Type != ret.Type {
+		t.Fatalf("Appended pipeline is not present in active pipelines.")
+	}
+
+}
+
 func TestRemove(t *testing.T) {
 	ap := NewActivePipelines()
 
@@ -36,6 +54,96 @@ func TestRemove(t *testing.T) {
 
 	if count != 1 {
 		t.Fatalf("Expected pipeline count to be %v. Got %v.", 1, count)
+	}
+}
+
+func TestGetByName(t *testing.T) {
+	ap := NewActivePipelines()
+
+	p1 := gaia.Pipeline{
+		Name:    "Pipeline A",
+		Type:    gaia.PTypeGolang,
+		Created: time.Now(),
+	}
+	ap.Append(p1)
+
+	ret := ap.GetByName("Pipeline A")
+
+	if p1.Name != ret.Name || p1.Type != ret.Type {
+		t.Fatalf("Pipeline A should have been retrieved.")
+	}
+
+	ret = ap.GetByName("Pipeline B")
+	if ret != nil {
+		t.Fatalf("Pipeline B should not have been retrieved.")
+	}
+}
+
+func TestReplace(t *testing.T) {
+	ap := NewActivePipelines()
+
+	p1 := gaia.Pipeline{
+		Name: "Pipeline A",
+		Type: gaia.PTypeGolang,
+		Repo: gaia.GitRepo{
+			URL:       "https://github.com/gaia-pipeline/go-test-example-1",
+			LocalDest: "tmp",
+		},
+		Created: time.Now(),
+	}
+	ap.Append(p1)
+
+	p2 := gaia.Pipeline{
+		Name: "Pipeline A",
+		Type: gaia.PTypeGolang,
+		Repo: gaia.GitRepo{
+			URL:       "https://github.com/gaia-pipeline/go-test-example-2",
+			LocalDest: "tmp",
+		},
+		Created: time.Now(),
+	}
+	ap.Append(p2)
+
+	ret := ap.Replace(p2)
+	if !ret {
+		t.Fatalf("The pipeline could not be replaced")
+	}
+
+	p := ap.GetByName("Pipeline A")
+	if p.Repo.URL != "https://github.com/gaia-pipeline/go-test-example-2" {
+		t.Fatalf("The pipeline repo URL should have been replaced")
+	}
+}
+
+func TestIter(t *testing.T) {
+	ap := NewActivePipelines()
+
+	var pipelineNames = []string{"Pipeline A", "Pipeline B", "Pipeline C"}
+	var retrievedNames []string
+
+	for _, n := range pipelineNames {
+		p := gaia.Pipeline{
+			Name:    n,
+			Type:    gaia.PTypeGolang,
+			Created: time.Now(),
+		}
+		ap.Append(p)
+	}
+
+	count := 0
+	for pipeline := range ap.Iter() {
+		count++
+		retrievedNames = append(retrievedNames, pipeline.Name)
+	}
+
+	if count != len(pipelineNames) {
+		t.Fatalf("Expected %d pipelines. Got %d.", len(pipelineNames), count)
+	}
+
+	for i := range retrievedNames {
+		if pipelineNames[i] != retrievedNames[i] {
+			t.Fatalf("The pipeline names do not match")
+		}
 	}
 }
 
