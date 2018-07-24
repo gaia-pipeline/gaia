b42cbd145d01bc199f999796a623df05b9358534
Gergely Brautigam <skarlso777@gmail.com>
2018-07-24 07:27:01 +0200

    More work on the api.

--- security/vault.go
+++ security/vault.go
@@ -1,21 +1,47 @@
 package security
 
-// CreateVault creates the vault file which is a simple k/v storage medium with AES encryption.
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/gaia-pipeline/gaia"
+)
+
+const (
+	vaultName = ".gaia_vault"
+)
+
+// Vault is a secret storage for data that gaia needs to store encrypted.
+type Vault struct {
+	File *os.File
+	Path string
+	Data map[string]interface{}
+}
+
+// NewVault creates a vault which is a simple k/v storage medium with AES encryption.
 // The format is:
 // KEY=VALUE
 // KEY2=VALUE2
-func CreateVault() error {
-	return nil
+func NewVault() (*Vault, error) {
+	v := new(Vault)
+	vaultPath := filepath.Join(gaia.Cfg.HomePath, vaultName)
+	f, err := os.Create(vaultPath)
+	if err != nil {
+		gaia.Cfg.Logger.Error("failed creating vault file: ", err.Error())
+		return nil, err
+	}
+	v.File = f
+	v.Data = make(map[string]interface{}, 0)
+	return v, nil
 }
 
 // OpenVault decrypts the contents of the vault and fills up a map of data to work with.
-func OpenVault(password string) map[string]interface{} {
-	var vault map[string]interface{}
-	return vault
+func (v *Vault) OpenVault(password string) error {
+	return nil
 }
 
 // CloseVault encrypts data passed to the vault in a k/v format.
-func CloseVault(data map[string]interface{}, password string) error {
+func (v *Vault) CloseVault() error {
 	return nil
 }
 
