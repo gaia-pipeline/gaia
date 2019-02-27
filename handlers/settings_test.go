29e00aa5a59c1bc87d8a33fec620674f215d3fca
Gergely Brautigam <gergely.brautigam@acquia.com>
2019-02-27 13:05:55 +0100

    Increased coverage and added ticket tests as well.

--- handlers/settings_test.go
+++ handlers/settings_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -10,6 +11,10 @@ import (
 	hclog "github.com/hashicorp/go-hclog"
 	"github.com/labstack/echo"
 )
+
+type status struct {
+	Status bool
+}
 
 func TestSetPollerToggle(t *testing.T) {
 	tmp, _ := ioutil.TempDir("", "TestSetPollerON")
@@ -70,6 +75,74 @@ func TestSetPollerToggle(t *testing.T) {
 		retStatus := http.StatusBadRequest
 		if rec.Code != retStatus {
 			t.Fatalf("expected response code %v got %v", retStatus, rec.Code)
+		}
+	})
+	t.Run("switching it off while the setting is on should pass", func(t *testing.T) {
+		gaia.Cfg = &gaia.Config{
+			Logger:       hclog.NewNullLogger(),
+			DataPath:     dataDir,
+			HomePath:     dataDir,
+			PipelinePath: dataDir,
+			Poll:         true,
+		}
+		req := httptest.NewRequest(echo.POST, "/", nil)
+		req.Header.Set("Content-Type", "application/json")
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		c.SetPath("/api/" + gaia.APIVersion + "/setttings/poll/off")
+
+		SettingsPollOff(c)
+		retStatus := http.StatusOK
+		if rec.Code != retStatus {
+			t.Fatalf("expected response code %v got %v", retStatus, rec.Code)
+		}
+		if gaia.Cfg.Poll != false {
+			t.Fatalf("poll value should have been set to false. was: %v", gaia.Cfg.Poll)
+		}
+	})
+	t.Run("switching it off while the setting is off should fail", func(t *testing.T) {
+		gaia.Cfg = &gaia.Config{
+			Logger:       hclog.NewNullLogger(),
+			DataPath:     dataDir,
+			HomePath:     dataDir,
+			PipelinePath: dataDir,
+			Poll:         false,
+		}
+		req := httptest.NewRequest(echo.POST, "/", nil)
+		req.Header.Set("Content-Type", "application/json")
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		c.SetPath("/api/" + gaia.APIVersion + "/setttings/poll/off")
+
+		SettingsPollOff(c)
+		retStatus := http.StatusBadRequest
+		if rec.Code != retStatus {
+			t.Fatalf("expected response code %v got %v", retStatus, rec.Code)
+		}
+	})
+	t.Run("getting the value should return the correct setting", func(t *testing.T) {
+		gaia.Cfg = &gaia.Config{
+			Logger:       hclog.NewNullLogger(),
+			DataPath:     dataDir,
+			HomePath:     dataDir,
+			PipelinePath: dataDir,
+			Poll:         true,
+		}
+		req := httptest.NewRequest(echo.POST, "/", nil)
+		req.Header.Set("Content-Type", "application/json")
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		c.SetPath("/api/" + gaia.APIVersion + "/setttings/poll/on")
+
+		SettingsPollGet(c)
+		retStatus := http.StatusOK
+		if rec.Code != retStatus {
+			t.Fatalf("expected response code %v got %v", retStatus, rec.Code)
+		}
+		var s status
+		json.NewDecoder(rec.Body).Decode(&s)
+		if s.Status != true {
+			t.Fatalf("expected returned status to be true. was: %v", s.Status)
 		}
 	})
 }
