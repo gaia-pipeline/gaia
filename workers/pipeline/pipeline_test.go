2f26ee2af86763d2a03d47454f86938bf7629a4f
Gergely Brautigam <gergely.brautigam@acquia.com>
2019-02-28 21:15:08 +0100

    Failed to save the file...

--- workers/pipeline/pipeline_test.go
+++ workers/pipeline/pipeline_test.go
@@ -74,7 +74,7 @@ func TestRemove(t *testing.T) {
 	ap.Remove(1)
 
 	count := 0
-	for _, pipeline := range ap.Iter() {
+	for _, pipeline := range ap.GetAll() {
 		count++
 		if pipeline.Name == "Pipeline B" {
 			t.Fatalf("Pipeline B still exists. It should have been removed.")
@@ -185,7 +185,7 @@ func TestIter(t *testing.T) {
 	}
 
 	count := 0
-	for _, pipeline := range ap.Iter() {
+	for _, pipeline := range ap.GetAll() {
 		count++
 		retrievedNames = append(retrievedNames, pipeline.Name)
 	}
@@ -246,7 +246,7 @@ func TestRemoveDeletedPipelines(t *testing.T) {
 
 	ap.RemoveDeletedPipelines(existingPipelineNames)
 
-	for _, pipeline := range ap.Iter() {
+	for _, pipeline := range ap.GetAll() {
 		if pipeline.Name == "Pipeline B" {
 			t.Fatalf("Pipeline B still exists. It should have been removed.")
 		}
