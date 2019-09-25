5a9fde8da0232155f2b7eef4cbdf81a3bbde1740
Michel Vocks <michelvocks@gmail.com>
2019-09-25 10:29:19 +0200

    Fixed wrong comment

--- helper/pipelinehelper/pipelinehelper.go
+++ helper/pipelinehelper/pipelinehelper.go
@@ -16,7 +16,7 @@ func GetRealPipelineName(name string, pType gaia.PipelineType) string {
 	return strings.TrimSuffix(name, typeDelimiter+pType.String())
 }
 
-// appendTypeToName appends the type to the output binary name.
+// AppendTypeToName appends the type to the output binary name.
 // This allows later to define the pipeline type by the pipeline binary name.
 func AppendTypeToName(n string, pType gaia.PipelineType) string {
 	return fmt.Sprintf("%s%s%s", n, typeDelimiter, pType.String())
