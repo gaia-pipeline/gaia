93c1346bb70da89b2b1faf19c0fb9828223ce8e3
Gergely Brautigam <gergely@arangodb.com>
2020-06-08 11:36:48 +0200

    Updated to the latest version for grpc

--- docker/Dockerfile.cpp
+++ docker/Dockerfile.cpp
@@ -6,7 +6,7 @@ RUN apt-get update && apt-get install -y \
     && apt-get clean
 
 # install protobuf first, then grpc
-ENV GRPC_RELEASE_TAG v1.28.x
+ENV GRPC_RELEASE_TAG v1.29.x
 RUN git clone -b ${GRPC_RELEASE_TAG} https://github.com/grpc/grpc /var/local/git/grpc && \
 	            cd /var/local/git/grpc && \
     git submodule update --init && \
