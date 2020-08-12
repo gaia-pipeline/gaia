45173738c6b4428965c62bc94c4c7118341069c5
Gergely Brautigam <gergely@arangodb.com>
2020-08-12 17:06:50 +0200

    Added a missing one

--- handlers/permission.go
+++ handlers/permission.go
@@ -8,6 +8,11 @@ import (
 )
 
 // PermissionGetAll simply returns a list of all the roles available.
+// @Summary Returns a list of default roles.
+// @Description Returns a list of all the roles available.
+// @Tags rbac
+// @Success 200 {array} gaia.UserRoleCategory
+// @Router /permission [get]
 func PermissionGetAll(c echo.Context) error {
 	return c.JSON(http.StatusOK, rolehelper.DefaultUserRoles)
 }
